bced602d771b726d6fd360c92a09d7cfcab0284b
Boncheol Gu <boncheol.gu@gmail.com>
2017-08-26 16:55:19 +0900

    .

--- Poly.v
+++ Poly.v
@@ -316,5 +316,5 @@ Check @prod_uncurry.
 Theorem uncurry_curry : forall (X Y Z : Type) (f : X -> Y -> Z) x y,
     prod_curry (prod_uncurry f) x y = f x y.
 Proof.
-  intros X Y Z f x y.
-  
+  reflexivity.
+Qed.
